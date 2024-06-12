a4c7bd13a8a2555421cf6697b391e5103f3f2900
Finley Gibson <f.j.gibson@exeter.ac.uk>
2024-06-12 15:43:35 +0100

    added rsquared measure to measures.py

--- src/datan/measure.py
+++ src/datan/measure.py
@@ -1,4 +1,5 @@
 import numpy.typing as npt
+from sklearn.metrics import r2_score
 
 
 def accuracy(y: npt.NDArray[float], y_: npt.NDArray[float]) -> float:
@@ -8,3 +9,12 @@ def accuracy(y: npt.NDArray[float], y_: npt.NDArray[float]) -> float:
     """
     assert y.shape == y_.shape
     return ((y == y_).sum()/len(y))*100
+
+
+def r_squared(y: npt.NDArray[float], y_: npt.NDArray[float]) -> float:
+    """
+    Simple function to report the accuracy percentage based on predited
+    values and real values.
+    """
+    assert y.shape == y_.shape
+    return r2_score(y, y_)
